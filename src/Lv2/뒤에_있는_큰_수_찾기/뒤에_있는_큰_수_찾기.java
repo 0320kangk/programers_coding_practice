11fd50833062e4cd40eaabb0f6603656f980dfc3
0320kangk <0320kangk@naver.com>
2023-05-30 16:30:14 +0900

    Revert "Revert "뒤에 있는 큰 수 찾기 코드 및 풀이""
    This reverts commit f3a41dca018976b6682ed755f4f71fc0c02ca1eb.

--- src/Lv2/뒤에_있는_큰_수_찾기/뒤에_있는_큰_수_찾기.java
+++ src/Lv2/뒤에_있는_큰_수_찾기/뒤에_있는_큰_수_찾기.java
@@ -1,2 +1,25 @@
-package Lv2.뒤에_있는_큰_수_찾기;public class 뒤에_있는_큰_수_찾기 {
+package Lv2.뒤에_있는_큰_수_찾기;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class 뒤에_있는_큰_수_찾기 {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        //{2,3,3,5}
+        Stack<Integer> nums = new Stack<>();
+        nums.add(0);
+        for (int i = 1; i < numbers.length; i++) {
+            while ( !nums.isEmpty() && numbers[nums.peek()] < numbers[i] ) {
+                int index = nums.pop();
+                answer[index] = numbers[i];
+            }
+            nums.add(i);
+        }
+        for (int index : nums) {
+            answer[index] = -1;
+        }
+        return answer;
+    }
 }
