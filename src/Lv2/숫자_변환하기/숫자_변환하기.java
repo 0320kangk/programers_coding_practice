491409745d381b5bf9d890d4659a40781647c8e9
0320kangk <0320kangk@naver.com>
2023-05-31 18:13:44 +0900

    숫자 변환하기 다시 커밋

--- src/Lv2/숫자_변환하기/숫자_변환하기.java
+++ src/Lv2/숫자_변환하기/숫자_변환하기.java
@@ -1,2 +1,37 @@
-package Lv2.숫자_변환하기;public class 숫자_변환하기 {
+package Lv2.숫자_변환하기;
+import java.util.*;
+public class 숫자_변환하기 {
+    public int solution(int x, int y, int n) {
+        int answer = -1;
+        List<Set<Integer>> sets = new ArrayList<>();
+        int count = 0;
+        int min = 0;
+        Set<Integer> firstSet = new HashSet();
+        firstSet.add(x);
+        sets.add(firstSet);
+        while(min < y){
+            Set<Integer> newSet = new HashSet();
+            Set<Integer> preSet = sets.get(count);
+            min = Integer.MAX_VALUE;
+            for (Integer xValue : preSet) {
+                if (min > xValue) {
+                    min = xValue;
+                } if ( xValue == y) {
+                    return count;
+                }
+                newSet.add(xValue + n);
+                newSet.add(xValue * 2);
+                newSet.add(xValue * 3);
+            }
+            count++;
+            sets.add(newSet);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        숫자_변환하기 숫자_변환하기 = new 숫자_변환하기();
+        int solution = 숫자_변환하기.solution(1, 1, 1);
+        System.out.println(solution);
+    }
 }
