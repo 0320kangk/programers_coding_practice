c215b9589f090b1f91b470127fe4d92f72fd1068
0320kangk <0320kangk@naver.com>
2023-06-12 00:01:22 +0900

    마법의 엘리베이터 코드 및 풀이

---
+++ src/Lv2/마법의_엘리베이터/마법의_엘리베이터1.java
@@ -0,0 +1,43 @@
+package Lv2.마법의_엘리베이터;
+
+import java.util.Stack;
+
+public class 마법의_엘리베이터 {
+
+    public int solution(int storey) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        while (storey !=0 ) {
+            stack.add( storey % 10);
+            storey /= 10;
+        }
+        int add = 0;
+        int size = stack.size();
+        for (int i = 0; i < size; i++) {
+            Integer num1 = stack.pop() + add;
+            if ( num1 == 5 ){
+                answer += 5;
+                add = 0;
+                if ( !stack.isEmpty() && stack.peek() >= 5 ) {
+                    add = 1;
+                }
+            } else if ( num1 < 5){
+                answer += num1;
+                add = 0;
+            } else {
+                answer += 10 - num1;
+                add = 1;
+            }
+        }
+        answer += add;
+        return answer;
+    }
+
+
+
+    public static void main(String[] args) {
+        마법의_엘리베이터 마법의_엘리베이터 = new 마법의_엘리베이터();
+        int solution = 마법의_엘리베이터.solution(154);
+        System.out.println(solution);
+    }
+}
---
+++ src/Lv2/마법의_엘리베이터/마법의_엘리베이터2.java
@@ -0,0 +1,2 @@
+package Lv2.마법의_엘리베이터;public class 마법의_엘리베이터2 {
+}
