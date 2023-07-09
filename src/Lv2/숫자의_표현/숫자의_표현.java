1de8769576633d30cedced19ed0c565a4b47cd9c
0320kangk <0320kangk@naver.com>
2023-07-09 17:25:18 +0900

    숫자의 표현 코드및 풀이

---
+++ src/Lv2/숫자의_표현/숫자의_표현.java
@@ -0,0 +1,20 @@
+package Lv2.숫자의_표현;
+
+public class 숫자의_표현 {
+    public int solution(int n) {
+        int answer = 1;
+        for (int i = 1; i <= Math.floor(n/2.0) ; i++) {
+            int x = i;
+            int temp = x;
+            while (true) {
+                x += temp;
+                temp++;
+                if ( x== n)
+                    answer++;
+                else if ( x > n)
+                    break;
+            }
+        }
+        return answer;
+    }
+}
