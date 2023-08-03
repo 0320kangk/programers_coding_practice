fa4ee8db0c3f9936f12189fc056a189c96f9ab06
DESKTOP-6V1BU0M\kang <0320kangk@naver.com>
2023-08-03 16:11:20 +0900

    쿼드 압축 후 개수 세기 코드 및 풀이

--- src/Lv2/쿼드압축_후_개수_세기/쿼드압축_후_개수_세기.java
+++ src/Lv2/쿼드압축_후_개수_세기/쿼드압축_후_개수_세기.java
@@ -1,2 +1,34 @@
-package Lv2.쿼드압축_후_개수_세기;public class 쿼드압축_후_개수_세기 {
+package Lv2.쿼드압축_후_개수_세기;
+public class 쿼드압축_후_개수_세기 {
+    int one = 0, zero = 0;
+    public int[] solution(int[][] arr) {
+        int[] answer = {zero, one};
+        divide(arr, 0, arr.length, 0, arr.length);
+        return answer;
+    }
+
+    void divide(int[][] arr, int xStart, int xEnd, int yStart, int yEnd) {
+        int oneCount = 0;
+        int max = (int)Math.pow(xEnd - xStart, 2);
+        for (int i = xStart; i < xEnd; i++) {
+            for (int j = yStart; j < yEnd; j++) {
+                oneCount += arr[i][j];
+            }
+        }
+        if (oneCount == 0) zero++;
+        else if (oneCount == max) one++;
+        else {
+            int xMid = (xStart + xEnd) / 2;
+            int yMid = (yStart + yEnd) / 2;
+            divide(arr, xStart, xMid, yStart, yMid); //1
+            divide(arr, xStart, xMid, yMid, yEnd); //2
+            divide(arr, xMid, xEnd, yStart, yMid);
+            divide(arr, xMid, xEnd, yMid, yEnd);
+        }
+    }
+
+    public static void main(String[] args) {
+        쿼드압축_후_개수_세기 쿼드압축_후_개수_세기 = new 쿼드압축_후_개수_세기();
+        쿼드압축_후_개수_세기.solution(new int[][]{{1,1,0,0},{1,0,0,0},{1,0,0,1},{1,1,1,1}});
+    }
 }
