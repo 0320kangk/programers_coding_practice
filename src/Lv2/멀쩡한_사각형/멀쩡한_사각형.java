e27497c4a22d93ff5de352492fe9535ab99b405b
0320kangk <0320kangk@naver.com>
2023-08-09 22:36:34 +0900

    멀쩡한 사각형 쿼드압축 코드

--- src/Lv2/멀쩡한_사각형/멀쩡한_사각형.java
+++ src/Lv2/멀쩡한_사각형/멀쩡한_사각형.java
@@ -1,2 +1,25 @@
-package Lv2.멀쩡한_사각형;public class 멀쩡한_사각형 {
+package Lv2.멀쩡한_사각형;
+
+public class 멀쩡한_사각형 {
+
+    public long solution(int w, int h) {
+        long answer = 1;
+
+        long count = w + h - 2 + 1; //0,4 3,0
+        for (int x = 1; x < w; x++) {
+            double y = ( 1- (x / (double) w)) * h; //
+            System.out.println(y);
+            if ( y == (int) y ) {
+                count--;
+            }
+        }
+        answer = w * h - count;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        멀쩡한_사각형 멀쩡한_사각형 = new 멀쩡한_사각형();
+        long solution = 멀쩡한_사각형.solution(3, 4);
+        System.out.println(solution);
+    }
 }
--- src/Lv2/쿼드압축_후_개수_세기/쿼드압축_후_개수_세기.java
+++ src/Lv2/쿼드압축_후_개수_세기/쿼드압축_후_개수_세기.java
@@ -5,7 +5,6 @@ public class 쿼드압축_후_개수_세기 {
     public int[] solution(int[][] arr) {
         int[] answer = {zero, one};
         divide(arr, 0, arr.length, 0, arr.length);
-        
         return answer;
     }
 
